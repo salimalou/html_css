f38675c2a044d907984d2372f9d526d60e49e092
salima <salima.louisor@lepoles.org>
2022-04-29 16:52:55 +0200

    29

---
+++ js/06comparaisons.js
@@ -0,0 +1,23 @@
+// var int = 5;
+// // window.alert("bonjour, je m'appel Salima");
+// console.log(int);
+
+// var salima = "2003";
+// var calcul = int + salima;
+// document.write(calcul + "<br>");
+
+// var unBooleen = false;
+// var intr1;
+// var euro = prompt("valeur en dollar");
+// var convert_dollar = euro * 1.0531;
+// document.write(convert_dollar);
+
+// prompt("valeur");
+// var valeur = document.write("document");
+// console.log(valeur);
+
+var nb1 = 10;
+var nb2 = "20";
+
+nb1 = nb1.toString();
+console.log(typeof nb1);
