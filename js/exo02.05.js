6b25d79d65e41fa08cbee3b16b1bb84692a0d793
salima <salima.louisor@lepoles.org>
2022-05-03 16:56:54 +0200

    03

---
+++ js/exo02.05.js
@@ -0,0 +1,70 @@
+// var nom;
+// nom = "louisor";
+// // alert(nom);
+// console.log(nom);
+// prompt("Question: ?", nom);
+
+// var jour;
+// jour = prompt();
+// console.log(jour);
+// document.write("louisor salima");
+
+// var monExercice = "je suis entrain de faire mon exercice";
+// // document.write(monExercice);
+// console.log(monExercice);
+// console.log(typeof monExercice);
+// // alert(monExercice);
+// monExercice = prompt("", "");
+// document.write(monExercice);
+
+// var monChiffre = 2022;
+// document.write(typeof monChiffre);
+
+// var monChiffreDecimal = 3.14;
+// document.write(typeof monChiffreDecimal);
+
+// var maChaineDeCaracteres = "57.12";
+// console.log(maChaineDeCaracteres);
+// document.write(maChaineDeCaracteres + "&nbsp");
+
+// maChaineDeCaracteres = parseFloat(maChaineDeCaracteres);
+// document.write(maChaineDeCaracteres + "&nbsp");
+// console.log(maChaineDeCaracteres);
+
+// maChaineDeCaracteres = parseInt(maChaineDeCaracteres);
+// document.write(maChaineDeCaracteres + "&nbsp");
+// console.log(maChaineDeCaracteres);
+
+// maChaineDeCaracteres = maChaineDeCaracteres.toString();
+// document.write(maChaineDeCaracteres + "&nbsp");
+// console.log(maChaineDeCaracteres);
+
+// var annee = 2017;
+// future = 3;
+// calcule = annee + future;
+// console.log(calcule);
+
+// document.write(calcule + "&nbsp,&nbsp" + calcule + "</br>");
+// document.write(calcule + "<br>");
+
+var nb1 = 10;
+var nb2 = 5;
+var resultat = nb1 + nb2;
+document.write(resultat + "<br>");
+console.log(resultat);
+
+var resultatSoustraction = nb1 - nb2;
+document.write(resultatSoustraction + "<br>");
+console.log(resultatSoustraction);
+
+var resultatMultiplication = nb1 * nb2;
+document.write(resultatMultiplication + "<br>");
+console.log(resultatMultiplication);
+
+var resultatDivison = nb1 / nb2;
+document.write(resultatDivison + "<br>");
+console.log(resultatDivison);
+
+var resultatModulo = nb1 % nb2;
+document.write(resultatModulo + "<br>");
+console.log(resultatModulo);
---
+++ reference/reference.js/.js
@@ -0,0 +1,2 @@
+var nom;
+nom = louisor;
