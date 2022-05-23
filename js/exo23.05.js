ef36562da112a64c8a3108568399bf006cc312e2
salima <salima.louisor@lepoles.org>
2022-05-23 16:56:50 +0200

    23

---
+++ js/exo23.05.js
@@ -0,0 +1,8 @@
+var resultat = isNaN(tableauFruit);
+tableauFruit = [];
+for (var i = 0; i < 5; i++) {
+  tableauFruit[i] = prompt("fruits?");
+  isNaN();
+  console.log(resultat);
+  document.write(tableauFruit[i] + "<br>");
+}
